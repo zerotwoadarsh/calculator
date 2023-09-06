782d17b45a5d0397a264669d24670279587549a2
zerotwoadarsh <124758307+zerotwoadarsh@users.noreply.github.com>
2023-09-06 21:08:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-let input = document.getElementById('inpitBox');
+let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
-arr.forEach(buttons =>{
-    button.addEventListener('click',(e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
+arr.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            try {
+                string = eval(string);
+                if (isNaN(string) || !isFinite(string)) {
+                    input.value = "Error";
+                } else {
+                    input.value = string;
+                }
+            } catch (error) {
+                input.value = "Error";
+            }
+        } else {
+            string += e.target.innerHTML;
             input.value = string;
         }
-
-        string += e.target.innerHTML;
-        input.value = string;
-    })
-})
+    });
+});
